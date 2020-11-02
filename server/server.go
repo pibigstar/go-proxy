f6fc269191effc7708c9394410633dc0c3c58429
QXQZX <501966782@qq.com>
2020-11-02 19:59:27 +0800

    modify last PR

--- server/server.go
+++ server/server.go
@@ -32,37 +32,44 @@ type client struct {
 }
 
 // 从Client端读取数据
-func (c *client) Read() {
+func (c *client) Read(ctx context.Context) {
 	// 如果10秒钟内没有消息传输，则Read函数会返回一个timeout的错误
 	_ = c.conn.SetReadDeadline(time.Now().Add(time.Second * 10))
 	for {
-		data := make([]byte, 10240)
-		n, err := c.conn.Read(data)
-		if err != nil && err != io.EOF {
-			if strings.Contains(err.Error(), "timeout") {
-				// 设置读取时间为3秒，3秒后若读取不到, 则err会抛出timeout,然后发送心跳
-				_ = c.conn.SetReadDeadline(time.Now().Add(time.Second * 3))
-				c.conn.Write([]byte("pi"))
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			data := make([]byte, 10240)
+			n, err := c.conn.Read(data)
+			if err != nil && err != io.EOF {
+				if strings.Contains(err.Error(), "timeout") {
+					// 设置读取时间为3秒，3秒后若读取不到, 则err会抛出timeout,然后发送心跳
+					_ = c.conn.SetReadDeadline(time.Now().Add(time.Second * 3))
+					c.conn.Write([]byte("pi"))
+					continue
+				}
+				fmt.Println("读取出现错误...")
+				c.exit <- err
+				return
+			}
+
+			// 收到心跳包,则跳过
+			if data[0] == 'p' && data[1] == 'i' {
+				fmt.Println("server收到心跳包")
 				continue
 			}
-			fmt.Println("读取出现错误...")
-			c.exit <- err
-			return
+			c.read <- data[:n]
 		}
-
-		// 收到心跳包,则跳过
-		if data[0] == 'p' && data[1] == 'i' {
-			fmt.Println("server收到心跳包")
-			continue
-		}
-		c.read <- data[:n]
 	}
 }
 
 // 将数据写入到Client端
-func (c *client) Write() {
+func (c *client) Write(ctx context.Context) {
 	for {
 		select {
+		case <-ctx.Done():
+			return
 		case data := <-c.write:
 			_, err := c.conn.Write(data)
 			if err != nil && err != io.EOF {
@@ -83,23 +90,30 @@ type user struct {
 }
 
 // 从User端读取数据
-func (u *user) Read() {
+func (u *user) Read(ctx context.Context) {
 	_ = u.conn.SetReadDeadline(time.Now().Add(time.Second * 200))
 	for {
-		data := make([]byte, 10240)
-		n, err := u.conn.Read(data)
-		if err != nil && err != io.EOF {
-			u.exit <- err
+		select {
+		case <-ctx.Done():
 			return
+		default:
+			data := make([]byte, 10240)
+			n, err := u.conn.Read(data)
+			if err != nil && err != io.EOF {
+				u.exit <- err
+				return
+			}
+			u.read <- data[:n]
 		}
-		u.read <- data[:n]
 	}
 }
 
 // 将数据写给User端
-func (u *user) Write() {
+func (u *user) Write(ctx context.Context) {
 	for {
 		select {
+		case <-ctx.Done():
+			return
 		case data := <-u.write:
 			_, err := u.conn.Write(data)
 			if err != nil && err != io.EOF {
@@ -160,11 +174,10 @@ func main() {
 }
 
 func HandleClient(client *client, userConnChan chan net.Conn) {
-
-	go client.Read()
-	go client.Write()
-
 	ctx, cancel := context.WithCancel(context.Background())
+
+	go client.Read(ctx)
+	go client.Write(ctx)
 
 	for {
 		select {
@@ -175,10 +188,8 @@ func HandleClient(client *client, userConnChan chan net.Conn) {
 				write: make(chan []byte),
 				exit:  make(chan error),
 			}
-			go user.Read()
-			go user.Write()
 
-			go handle(client, user, ctx)
+			go handle(ctx, client, user)
 		case err := <-client.exit:
 			fmt.Println("client出现错误, 关闭连接", err.Error())
 			cancel()
@@ -191,7 +202,11 @@ func HandleClient(client *client, userConnChan chan net.Conn) {
 // 将两个Socket通道链接
 // 1. 将从user收到的信息发给client
 // 2. 将从client收到信息发给user
-func handle(client *client, user *user, ctx context.Context) {
+func handle(ctx context.Context, client *client, user *user) {
+	userCtx, cancel := context.WithCancel(ctx)
+	go user.Read(userCtx)
+	go user.Write(userCtx)
+
 	for {
 		select {
 		case userRecv := <-user.read:
@@ -204,11 +219,13 @@ func handle(client *client, user *user, ctx context.Context) {
 		case <-ctx.Done():
 			_ = client.conn.Close()
 			_ = user.conn.Close()
+			cancel() // client关闭，关闭user读写协程
 			return
 
 		case err := <-user.exit:
 			fmt.Println("user出现错误，关闭连接", err.Error())
 			_ = user.conn.Close()
+			cancel() // user错误，关闭user读写协程
 			return
 		}
 	}
